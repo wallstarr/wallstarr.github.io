b81abe795d82113c8bef5f199b5bdfd65447ada5
Dan Blustein <dansakai0@gmail.com>
2021-05-18 15:49:30 +0900

    made about section responsive

--- src/components/AboutObject.js
+++ src/components/AboutObject.js
@@ -1,17 +1,33 @@
 function AboutObject() {
+  return (
+    <span class="bg-gray-900 hover:bg-gray-700 text-customwhite font-mono min-w-max text-supersmall items-center justify-center verytiny:text-xxxs tiny:text-xxs sm:text-xs md:text-sm rounded-xl p-2 md:p-4 md:mr-16 xl:mr-24 mt-4 md:mt-4 xl:mt-12 xl:text-base mb-3 md:mb-6 xl:mb-4">
+      <div>
+        <span class="text-gray-600">1</span>&nbsp;&nbsp;&#123;
+      </div>
+      <div>
+        <span class="text-gray-600">2</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span class="text-customyellow">“name”</span>: “Dan Blustein”,
+      </div>
+      <div>
+        <span class="text-gray-600">3</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span class="text-customyellow">“developing”</span>: “iOS Widget
+        tracking Japan's COVID",
+      </div>
+      <div>
+        <span class="text-gray-600">4</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span class="text-customyellow">“watching”</span>: [“Monster”,
+        “Mushishi”],
+      </div>
+      <div>
+        <span class="text-gray-600">5</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span class="text-customyellow">“status”</span>: “hoping to work at
+        somewhere awesome”
+      </div>
+      <div>
+        <span class="text-gray-600">6</span>&nbsp;&nbsp;&#125;
+      </div>
+    </span>
+  );
+}
 
-    return (
-        
-            <span class="bg-gray-900 text-customwhite font-mono min-w-max text-supersmall items-center justify-center verytiny:text-xxxs tiny:text-xxs sm:text-xs md:text-sm rounded-xl p-2 md:p-4 md:mr-16 xl:mr-24 mt-4 md:mt-4 xl:mt-12 xl:text-base mb-16 xl:mb-4">
-                <div><span class="text-gray-600">1</span>&nbsp;&nbsp;&#123;</div>
-                <div><span class="text-gray-600">2</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="text-customyellow">“name”</span>: “Dan Blustein”,</div>
-                <div><span class="text-gray-600">3</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="text-customyellow">“developing”</span>: “iOS Widget tracking Japan's COVID",</div>
-                <div><span class="text-gray-600">4</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="text-customyellow">“watching”</span>: [“Monster”, “Mushishi”],</div>
-                <div><span class="text-gray-600">5</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="text-customyellow">“status”</span>: “hoping to work at somewhere awesome”</div>
-                <div><span class="text-gray-600">6</span>&nbsp;&nbsp;&#125;</div>
-            </span>
-    );
-  }
-  
-  export default AboutObject;
-  
+export default AboutObject;
