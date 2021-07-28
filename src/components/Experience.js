5f5c1132f898a585e2c48af5e3e311868507ad77
Dan Blustein <dansakai0@gmail.com>
2021-07-29 04:41:48 +0900

    updated experience section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,7 +3,7 @@ function Experience() {
   return (
       <div class="relative pb-10 flex flex-col min-h-min items-center bg-customblack z-10 -mt-12 text-white" id="experience">
         <div class="text-4xl font-bold underline text-center">Experience</div>
-        <img width="400" src="https://i.ibb.co/2hsm2Cj/Screen-Shot-2021-05-18-at-16-16-15.png" alt=""></img>
+        <img width="400" src="https://www.seekpng.com/png/detail/137-1379223_work-in-progress-work-in-progress-computer.png" alt=""></img>
       </div>
   );
 }
