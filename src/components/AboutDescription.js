4e46ee53e472a0ab8d1ce2d3a327867ebfaf43fa
Dan Blustein <dansakai0@gmail.com>
2022-08-17 20:50:53 +0900

    updated about section

--- src/components/AboutDescription.js
+++ src/components/AboutDescription.js
@@ -3,8 +3,8 @@ function AboutDescription() {
       <div class="flex flex-col min-h-min text-left text-white">
         <div class="text-4xl font-bold mt-6 md:mt-16 mb-4 mx-4 md:mx-32 text-left underline">About</div>
         <div class="text-base verytiny:text-xl font-semibold mx-4 mb-4 md:mx-32">Hey, my name is Dan. I am a self-driven developer from America and Japan, currently in my 3rd year at the University of British Columbia studying Computer Science. My main interests lie in engineering products with purpose, being efficient and professional, and making the small things unforgettable. </div>
-        <div class="text-base verytiny:text-xl font-semibold mx-4 mb-4 md:mx-32">Outside of development and SENG, I love video games (Smash Bros and Rocket League), basketball (player and spectator), anime (<a class="text-blue-600 underline" href="https://myanimelist.net/profile/wallstarr" target="_blank" rel="noreferrer">too many to list</a>), and hanging w the homies ofc. </div>
-        <div class="text-base verytiny:text-xl font-semibold mx-4 mb-4 xl:mb-16 md:mx-32">Currently, I am an SRE Co-op for Visier Inc. from September 2021 through April 2022.</div>
+        <div class="text-base verytiny:text-xl font-semibold mx-4 mb-4 md:mx-32">Outside of development and SENG, I love video games (Smash Bros and Rocket League), basketball (player and spectator), anime (<a class="text-blue-600 underline" href="https://myanimelist.net/profile/wallstarr" target="_blank" rel="noreferrer">too many to list</a>), and going to the gym. </div>
+        <div class="text-base verytiny:text-xl font-semibold mx-4 mb-4 xl:mb-16 md:mx-32">I am currently seeking co-op/internship oppurtunities starting from either January, May, or September of 2023.</div>
       </div>
       
   );
