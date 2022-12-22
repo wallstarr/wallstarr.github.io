f36a562d7b1ac3685bcbe37dff6293bb49b68e06
Dan Blustein <dansakai0@gmail.com>
2022-12-21 20:20:18 -0800

    changes

--- src/components/HoverLetter.js
+++
@@ -1,9 +0,0 @@
-function HoverLetter({letter}) {
-  return (
-    <span class="text-customwhite font-extrabold hover:text-gray-500 transition-color ease-in-out duration-300">
-        {letter}
-  </span>
-  );
-}
-
-export default HoverLetter;
