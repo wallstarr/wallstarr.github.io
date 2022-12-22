9b35550ed68e60aa7f1505da35ce05f48506509e
Dan Blustein <dansakai0@gmail.com>
2022-12-21 20:59:01 -0800

    changes

---
+++ public/components/AboutObject.js
@@ -0,0 +1,30 @@
+function AboutObject() {
+  return (
+    <span class="bg-gray-900 hover:bg-gray-700 text-customwhite font-mono min-w-max text-supersmall items-center justify-center verytiny:text-xxxs tiny:text-xxs sm:text-xs md:text-sm rounded-xl p-2 md:p-4 md:mr-16 xl:mr-24 mt-4 md:mt-4 xl:mt-12 xl:text-base mb-3 md:mb-6 xl:mb-4">
+      <div>
+        <span class="text-gray-600">1</span>&nbsp;&nbsp;&#123;
+      </div>
+      <div>
+        <span class="text-gray-600">2</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span class="text-customyellow">“name”</span>: “Dan Blustein”,
+      </div>
+      <div>
+        <span class="text-gray-600">3</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span class="text-customyellow">“developing”</span>: “Poker Odds App",
+      </div>
+      <div>
+        <span class="text-gray-600">4</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span class="text-customyellow">“watching”</span>: ["Made in Abyss"],
+      </div>
+      <div>
+        <span class="text-gray-600">5</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <span class="text-customyellow">“status”</span>: “hoping to work on cool stuff”
+      </div>
+      <div>
+        <span class="text-gray-600">6</span>&nbsp;&nbsp;&#125;
+      </div>
+    </span>
+  );
+}
+
+export default AboutObject;
